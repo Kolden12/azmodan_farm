bd39eb14c4870334e2252ce6fce77c02a4ed6a31
Kolden12 <143414254+Kolden12@users.noreply.github.com>
2025-12-26 23:57:52 -0600

    Delete azmodan_farm-1.0.7 directory

--- azmodan_farm-1.0.7/core/tracker.lua
+++
@@ -1,9 +0,0 @@
-local plugin_label = 'azmodan_farm'
--- kept plugin label instead of waiting for update_tracker to set it
-
-local tracker = {
-    name        = plugin_label,
-    drop_sigils = false
-}
-
-return tracker
